11aa6d23fdef879c914f37e41b2793514441157c
Fabricio2407 <fabriciocduarte2413@gmail.com>
2026-07-26 15:31:16 -0300

    chore: models

---
+++ backend/app/models.py
@@ -0,0 +1,66 @@
+from typing import Literal
+
+from pydantic import BaseModel, Field, HttpUrl
+
+
+CategoriaObjeto = Literal[
+    "cozinha",
+    "banheiro",
+    "quarto",
+    "sala",
+    "limpeza",
+    "decoracao",
+    "eletrodomestico",
+    "movel",
+    "improvisado",
+    "amaldicoado",
+    "outro"
+]
+
+class ObjetoCriacao(BaseModel):
+    autor: str = Field(
+        min_length = 2,
+        max_length = 50
+    )
+
+    nome: str = Field(
+        min_length = 3,
+        max_length = 100
+    )
+
+    descricao: str = Field(
+        min_length = 3,
+        max_length = 500
+    )
+
+    categoria: CategoriaObjeto
+
+    imagem_url: HttpUrl
+
+
+class ObjetoAtualizado(BaseModel):
+    nome: str | None = Field(
+        default=None,
+        min_length=3,
+        max_length=100
+    )
+
+    descricao: str | None = Field(
+        default=None,
+        min_length=3,
+        max_length=500
+    )
+
+    categoria: str | None = None
+
+    imagem_url: HttpUrl | None = None
+
+
+class ObjetoResposta(BaseModel):
+    id: int
+    autor: str
+    nome: str
+    descricao: str
+    categoria: CategoriaObjeto
+    imagem_url: HttpUrl
+    curtidas: int
